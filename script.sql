1ca13c6d091b72a0a8e47adf64d24c40a03bca9b
Gulin Alexei <Arrakkis.ya@yandex.ru>
2026-01-05 12:07:40 +0300

    update docker-files, properties, test_api, script.sql, Report and Summary

--- script.sql
+++ script.sql
@@ -1,8 +1,28 @@
 -- "Показывает таблицу order_entity"
-SELECT * FROM order_entity;
+SELECT * FROM order_entity LIMIT 10;
 
 -- "Показывает таблицу credit_request_entity"
-SELECT * FROM credit_request_entity;
+SELECT * FROM credit_request_entity LIMIT 10;
 
--- "Показывает таблицу credit_request_entity"
-SELECT * FROM payment_entity;
+-- "Показывает таблицу payment_entity"
+SELECT * FROM payment_entity LIMIT 10;
+
+-- "Показывает одобренные запросы из таблицы payment_entity"
+SELECT * FROM payment_entity
+WHERE status = 'APPROVED'
+LIMIT 10;
+
+-- "Показывает отклонённые запросы из таблицы payment_entity"
+SELECT * FROM payment_entity
+WHERE status = 'DECLINED'
+LIMIT 10;
+
+-- "Показывает одобренные запросы из таблицы credit_request_entity"
+SELECT * FROM credit_request_entity
+WHERE status = 'APPROVED'
+LIMIT 10;
+
+-- "Показывает отклонённые запросы из таблицы credit_request_entity"
+SELECT * FROM credit_request_entity
+WHERE status = 'DECLINED'
+LIMIT 10;
--- tests/test_api.py
+++ tests/test_api.py
@@ -15,9 +15,15 @@ def test_valid_pay (api_session, app_url):
         }
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/pay", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/pay", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
         assert response.status_code == 200
+
+    with allure.step("Получение ответа на post-запрос"):
+        error_message = response.json().get("status")
+    with allure.step("Сравнение ожидаемого ответа с реальным"):
+        assert error_message == "APPROVED", \
+            f"Ожидали сообщение 'APPROVED', получили '{error_message}'"
 
 
 @allure.epic("QA_Diplom")
@@ -34,9 +40,15 @@ def test_valid_credit (api_session, app_url):
         }
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/credit", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/credit", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
         assert response.status_code == 200
+
+    with allure.step("Получение ответа на post-запрос"):
+        error_message = response.json().get("status")
+    with allure.step("Сравнение ожидаемого ответа с реальным"):
+        assert error_message == "APPROVED", \
+            f"Ожидали сообщение 'APPROVED', получили '{error_message}'"
 
 
 @allure.epic("QA_Diplom")
@@ -53,15 +65,15 @@ def test_invalid_pay (api_session, app_url):
         }
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/pay", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/pay", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
-        assert response.status_code == 400
+        assert response.status_code == 200
 
     with allure.step("Получение ответа на post-запрос"):
-        error_message = response.json().get("message")
+        error_message = response.json().get("status")
     with allure.step("Сравнение ожидаемого ответа с реальным"):
-        assert error_message == "Ошибка! Банк отказал в проведении операции.", \
-            f"Ожидали сообщение 'Ошибка! Банк отказал в проведении операции.', получили '{error_message}'"
+        assert error_message == 'DECLINED', \
+            f"Ожидали сообщение 'DECLINED', получили '{error_message}'"
 
 
 @allure.epic("QA_Diplom")
@@ -78,15 +90,15 @@ def test_invalid_credit (api_session, app_url):
         }
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/credit", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/credit", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
-        assert response.status_code == 400
+        assert response.status_code == 200
 
     with allure.step("Получение ответа на post-запрос"):
-        error_message = response.json().get("message")
+        error_message = response.json().get("status")
     with allure.step("Сравнение ожидаемого ответа с реальным"):
-        assert error_message == "Ошибка! Банк отказал в проведении операции.", \
-            f"Ожидали сообщение 'Ошибка! Банк отказал в проведении операции.', получили '{error_message}'"
+        assert error_message == 'DECLINED', \
+            f"Ожидали сообщение 'DECLINED', получили '{error_message}'"
 
 
 @allure.epic("QA_Diplom")
@@ -97,15 +109,15 @@ def test_empty_form_credit (api_session, app_url):
         payload = {}
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/credit", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/credit", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
         assert response.status_code == 400
 
     with allure.step("Получение ответа на post-запрос"):
         error_message = response.json().get("message")
     with allure.step("Сравнение ожидаемого ответа с реальным"):
-        assert error_message == "Заполните форму", \
-            f"Ожидали сообщение 'Заполните форму', получили '{error_message}'"
+        assert error_message == "Empty JSON", \
+            f"Ожидали сообщение 'Empty JSON', получили '{error_message}'"
 
 
 @allure.epic("QA_Diplom")
@@ -116,21 +128,21 @@ def test_empty_form_pay (api_session, app_url):
         payload = {}
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/pay", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/pay", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
         assert response.status_code == 400
 
     with allure.step("Получение ответа на post-запрос"):
         error_message = response.json().get("message")
     with allure.step("Сравнение ожидаемого ответа с реальным"):
-        assert error_message == "Заполните форму", \
-            f"Ожидали сообщение 'Заполните форму', получили '{error_message}'"
+        assert error_message == "Empty JSON", \
+            f"Ожидали сообщение 'Empty JSON', получили '{error_message}'"
 
 
 @allure.epic("QA_Diplom")
 @allure.feature("API-запросы")
 @allure.story("Отправка невалидного JSON-файла при покупке тура в кредит")
-def test_empty_form_credit (api_session, app_url):
+def test_invalid_json_credit (api_session, app_url):
     with allure.step("Невалидный JSON-файл"):
         payload = {
             "number": "qwerty",
@@ -141,7 +153,7 @@ def test_empty_form_credit (api_session, app_url):
         }
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/credit", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/credit", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
         assert response.status_code == 400
 
@@ -155,7 +167,7 @@ def test_empty_form_credit (api_session, app_url):
 @allure.epic("QA_Diplom")
 @allure.feature("API-запросы")
 @allure.story("Отправка невалидного JSON-файла при покупке тура")
-def test_empty_form_pay (api_session, app_url):
+def test_invalid_json_pay (api_session, app_url):
     with allure.step("Невалидный JSON-файл"):
         payload = {
             "number": "qwerty",
@@ -166,7 +178,7 @@ def test_empty_form_pay (api_session, app_url):
         }
 
     with allure.step("Создание post-запроса"):
-        response = api_session.post(f"{app_url}/api/v1/pay", params=payload)
+        response = api_session.post(f"{app_url}/api/v1/pay", json=payload)
     with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
         assert response.status_code == 400
 
@@ -175,3 +187,103 @@ def test_empty_form_pay (api_session, app_url):
     with allure.step("Сравнение ожидаемого ответа с реальным"):
         assert error_message == "Invalid JSON", \
             f"Ожидали сообщение 'Invalid JSON', получили '{error_message}'"
+
+
+@allure.epic("QA_Diplom")
+@allure.feature("API-запросы")
+@allure.story("Отправка невалидного JSON-файла при покупке тура")
+def test_invalid_month_pay (api_session, app_url):
+    with allure.step("Невалидный JSON-файл"):
+        payload = {
+            "number": "4444 4444 4444 4441",
+            "year": "26",
+            "month": "99",
+            "holder": "Ivan Ivanov",
+            "cvc": "999"
+        }
+
+    with allure.step("Создание post-запроса"):
+        response = api_session.post(f"{app_url}/api/v1/pay", json=payload)
+    with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
+        assert response.status_code == 400
+
+    with allure.step("Получение ответа на post-запрос"):
+        error_message = response.json().get("message")
+    with allure.step("Сравнение ожидаемого ответа с реальным"):
+        assert error_message == "Invalid month", \
+            f"Ожидали сообщение 'Invalid month', получили '{error_message}'"
+
+
+@allure.epic("QA_Diplom")
+@allure.feature("API-запросы")
+@allure.story("Покупка тура в кредит по тестовой карте с номером для одобрения")
+def test_invalid_month_credit (api_session, app_url):
+    with allure.step("JSON-файл с валидными данными"):
+        payload = {
+            "number": "4444 4444 4444 4441",
+            "year": "26",
+            "month": "99",
+            "holder": "Ivan Ivanov",
+            "cvc": "999"
+        }
+
+    with allure.step("Создание post-запроса"):
+        response = api_session.post(f"{app_url}/api/v1/credit", json=payload)
+    with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
+        assert response.status_code == 400
+
+    with allure.step("Получение ответа на post-запрос"):
+        error_message = response.json().get("message")
+    with allure.step("Сравнение ожидаемого ответа с реальным"):
+        assert error_message == "Invalid month", \
+            f"Ожидали сообщение 'Invalid month', получили '{error_message}'"
+
+
+@allure.epic("QA_Diplom")
+@allure.feature("API-запросы")
+@allure.story("Отправка невалидного JSON-файла при покупке тура")
+def test_invalid_year_pay (api_session, app_url):
+    with allure.step("Невалидный JSON-файл"):
+        payload = {
+            "number": "4444 4444 4444 4441",
+            "year": "22",
+            "month": "08",
+            "holder": "Ivan Ivanov",
+            "cvc": "999"
+        }
+
+    with allure.step("Создание post-запроса"):
+        response = api_session.post(f"{app_url}/api/v1/pay", json=payload)
+    with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
+        assert response.status_code == 400
+
+    with allure.step("Получение ответа на post-запрос"):
+        error_message = response.json().get("message")
+    with allure.step("Сравнение ожидаемого ответа с реальным"):
+        assert error_message == "Invalid month", \
+            f"Ожидали сообщение 'Invalid month', получили '{error_message}'"
+
+
+@allure.epic("QA_Diplom")
+@allure.feature("API-запросы")
+@allure.story("Покупка тура в кредит по тестовой карте с номером для одобрения")
+def test_invalid_year_credit (api_session, app_url):
+    with allure.step("JSON-файл с валидными данными"):
+        payload = {
+            "number": "4444 4444 4444 4441",
+            "year": "22",
+            "month": "08",
+            "holder": "Ivan Ivanov",
+            "cvc": "999"
+        }
+
+    with allure.step("Создание post-запроса"):
+        response = api_session.post(f"{app_url}/api/v1/credit", json=payload)
+    with allure.step("Сравнение ожидаемого статус-кода ответа с реальным"):
+        assert response.status_code == 400
+
+    with allure.step("Получение ответа на post-запрос"):
+        error_message = response.json().get("message")
+    with allure.step("Сравнение ожидаемого ответа с реальным"):
+        assert error_message == "Invalid month", \
+            f"Ожидали сообщение 'Invalid month', получили '{error_message}'"
